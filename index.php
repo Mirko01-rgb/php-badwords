58819cd0f816b5e9fa424a1573409941a0a42505
Mirko <mirkosbaglia2001@gmail.com>
2021-05-03 19:00:35 +0200

    Corretta indentazione

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
      <?php
        $textLorem = ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
        echo "$textLorem";
-       ?>
+      ?>
     </p>
 
 
@@ -27,8 +27,7 @@
      <?php
        $length = strlen($textLorem);
        echo "$length";
-
-       ?>
+      ?>
     </p>
 
     <!-- Paragrafo censurato -->
@@ -39,7 +38,7 @@
         $newSentence = str_replace($badword, '***', $textLorem);
         // var_dump( $badword, $newSentence);
         echo "$newSentence";
-       ?>
+      ?>
     </p>
 
   </body>
