62d0be3827d90ef6503da0e4d96d1ef73a216af6
Mirko <mirkosbaglia2001@gmail.com>
2021-05-03 18:56:45 +0200

    Esercizio terminato

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
   </head>
   <body>
 
-   <h1>
+   <!-- Paragrafo originale -->
+   <h1>Paragrafo originale</h1>
+   <p>
      <!-- Creare una variabile con un paragrafo di
      testo.
      Visualizzare a schermo il paragrafo con la
@@ -14,23 +16,31 @@
      badword passata in GET con tre *. -->
      <?php
        $textLorem = ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-       //echo "$textLorem";
+       echo "$textLorem";
+       ?>
+    </p>
 
-       $badword = $_GET["badword"];
-       $newSentence = str_replace($badword, '***', $textLorem);
 
-       // var_dump( $textLorem, $badword, $newSentence);
-
-        echo "$newSentence";
-      ?>
-    </h1>
-
-    <h1>
+    <!-- Lunghezza paragrafo -->
+    <h1> Lunghezza paragrafo</h1>
+    <p>
      <?php
        $length = strlen($textLorem);
        echo "$length";
 
-      ?>
-    </h1>
+       ?>
+    </p>
+
+    <!-- Paragrafo censurato -->
+    <h1>Paragrafo censurato</h1>
+    <p>
+      <?php
+        $badword = $_GET["badword"];
+        $newSentence = str_replace($badword, '***', $textLorem);
+        // var_dump( $badword, $newSentence);
+        echo "$newSentence";
+       ?>
+    </p>
+
   </body>
 </html>
